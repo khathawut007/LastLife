fabef9637706212607861d8c2b1e438d64801f6a
khathawut007 <78632160+khathawut007@users.noreply.github.com>
2021-12-16 12:24:08 +0700

    Add files via upload
    final

--- Program.cs
+++ Program.cs
@@ -1,47 +1,55 @@
 ﻿using System;
 
-namespace ปรับปรุง
+namespace finals
 {
     class Program
     {
+        static void AddNode(ref Node node)
+        {
+            Console.WriteLine("Input skill name:");
+            Console.WriteLine("Add dependency for Fire Bolt?");
+            char answer = char.Parse(Console.ReadLine());
+            if (answer == 'Y')
+            {
+                string message = Console.ReadLine();
+                node = new Question(message);
+                AddNode(ref (node as Question).Left);
+                AddNode(ref (node as Question).Right);
+            }
+            else
+            {
+                string message = Console.ReadLine();
+                node = new DeceaseNode(message);
+            }
+
+
+        }
         static void Main(string[] args)
         {
-            string decide = "y";
-            string selectFlower;
-            FlowerStore flowerStore = new FlowerStore();
-            do{
-                Console.WriteLine("Select number for buy flower :"); 
-                foreach (string i in flowerStore.flowerList){
-                    Console.Write((flowerStore.flowerList.IndexOf(i) + 1) + " "); 
-                    Console.WriteLine(i); 
-                } 
-            
-                selectFlower = Console.ReadLine();
-                
-                switch (selectFlower){
-                    case "1":
-                        flowerStore.addToCart(flowerStore.flowerList[0]);
-                        Console.WriteLine("Added "+ flowerStore.flowerList[0]);
-                        break;
-                    
-                    case "2":
-                        flowerStore.addToCart(flowerStore.flowerList[1]);
-                        Console.WriteLine("Added "+ flowerStore.flowerList[1]);
-                        break;
-                    
-                    default:
-                        Console.WriteLine("Not Added to cart. found select number of flower");
-                        break;
-                }
-            
-                    Console.WriteLine("You can stop this progress ? exit for >> exit << progress and press any key for continue");
-                    decide = Console.ReadLine();
-                    if(decide == "exit") {
-                        Console.Write("Current my cart");
-                        flowerStore.showCart();
-                    } 
-                }    
-               while (decide != "exit");      
+            Node root = null;
+            AddNode(ref root);
+
+            Node ptr = root;
+            while (true)
+            {
+                 Console.WriteLine(ptr.Message);
+                 if (ptr is Question)
+                 {
+                     char answer = char.Parse(Console.ReadLine());
+                     if (answer == 'Y')
+                     {
+                         ptr = (ptr as Question).Left;
+                     }
+                     else
+                     {
+                        ptr = (ptr as Question).Right;
+                     }
+                 }
+                 else
+                 {
+                     break;
+                 }
+            }
         }
     }
 }
